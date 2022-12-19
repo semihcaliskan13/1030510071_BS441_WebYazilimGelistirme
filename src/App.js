47b88696365e3b29d86c4c753a33d2952a3436fa
semihcaliskan13 <118068918+semihcaliskan13@users.noreply.github.com>
2022-12-19 18:09:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Commitdeneme
+          Commitdenemecommit
         </a>
       </header>
     </div>
